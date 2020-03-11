836978b83185c51718d54201276f95907654f56f
Evan Anger <devandanger@gmail.com>
2020-03-10 22:46:08 -0400

    Added synchronous example

--- app/src/main/java/com/mightystrong/queueingserviceexample/services/BackgroundIntentService.kt
+++ app/src/main/java/com/mightystrong/queueingserviceexample/services/BackgroundIntentService.kt
@@ -7,6 +7,17 @@ import java.util.*
 
 class BackgroundIntentService : IntentService("BackgroundIntentService") {
     override fun onHandleIntent(p0: Intent?) {
+        syncTask()
+    }
+    private fun syncTask() {
+        val guid = UUID.randomUUID().toString()
+        Log.d("Background service", "OnHandleStart $guid")
+        Log.d("Background service", "Thread work started $guid")
+        Thread.sleep(5000)
+        Log.d("Background service", "Thread work finished $guid")
+        Log.d("Background service", "OnHandleFinish $guid")
+    }
+    private fun asyncTask() {
         val guid = UUID.randomUUID().toString()
         Log.d("Background service", "OnHandleStart $guid")
         val workThread = BackgroundWorkThread(guid)
